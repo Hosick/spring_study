9635cc934d1be4770c1ddfca2004daea1576ad62
Hosick <ghtjr5000@naver.com>
2021-01-07 01:40:22 +0900

    rest api practice

--- freelec-springboot2-webservice/src/main/java/com/hosick/book/springboot/service/posts/PostsService.java
+++ freelec-springboot2-webservice/src/main/java/com/hosick/book/springboot/service/posts/PostsService.java
@@ -2,9 +2,12 @@ package com.hosick.book.springboot.service.posts;
 
 import com.hosick.book.springboot.domain.posts.Posts;
 import com.hosick.book.springboot.domain.posts.PostsRepository;
+import com.hosick.book.springboot.web.dto.PostsListResponseDto;
 import com.hosick.book.springboot.web.dto.PostsResponseDto;
 import com.hosick.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hosick.book.springboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,26 +15,34 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
-    private final PostsRepository postsRepository;
 
-    @Transactional
-    public Long save(PostsSaveRequestDto requestDto) {
-        return postsRepository.save(requestDto.toEntity()).getId();
+  private final PostsRepository postsRepository;
 
-    }
+  @Transactional
+  public Long save(PostsSaveRequestDto requestDto) {
+    return postsRepository.save(requestDto.toEntity()).getId();
 
-    @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-        posts.update(requestDto.getTitle(), requestDto.getContent());
+  }
 
-        return id;
-    }
+  @Transactional
+  public Long update(Long id, PostsUpdateRequestDto requestDto) {
+    Posts posts = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+    posts.update(requestDto.getTitle(), requestDto.getContent());
 
-    public PostsResponseDto findById(Long id) {
-        Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-        return new PostsResponseDto(entity);
-    }
+    return id;
+  }
+
+  public PostsResponseDto findById(Long id) {
+    Posts entity = postsRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+    return new PostsResponseDto(entity);
+  }
+
+  @Transactional
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
+  }
 }
